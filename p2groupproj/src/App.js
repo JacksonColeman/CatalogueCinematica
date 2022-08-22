f9bda0056519be2ad1b9e6385478c55d61bf0cf2
Jackson Coleman <wcoleman@colgate.edu>
2022-08-22 13:59:55 -0500

    Moved search to its own component, added film detail component code

--- p2groupproj/src/App.js
+++ p2groupproj/src/App.js
@@ -4,47 +4,20 @@ import './App.css';
 import About from "./components/About"
 import NavBar from "./components/NavBar"
 import FilmWatchMgr from "./components/FilmWatchMgr"
+import Search from './components/Search';
 
 
 function App() {
-  const [filmSearch, setFilmSearch] = useState("");
-  const [currentFilm, setCurrentFilm] = useState([]);
-  const [posterUrl, setPosterUrl] = useState("");
-
 
   console.log(About, NavBar, FilmWatchMgr)
   // Search API by Title
   //https://www.omdbapi.com/?t={name}&apikey=ecf3f0c5
 
-  const handleFilmSearch = (e) => {
-    let filmTitle = e.target.value;
-    setFilmSearch(filmTitle);
-    // replace spaces with underscores so URL can handle it
-  }
-
-  const urlifyTitle = (title) => {
-    title = title.replace(" ", "+")
-    return title;
-  }
-
-  const handleFilmSearchSubmit = (e) => {
-    e.preventDefault();
-    let urlifiedTitle = urlifyTitle(filmSearch);
-    fetch(`https://www.omdbapi.com/?t=${urlifiedTitle}&apikey=ecf3f0c5`)
-    .then(res => res.json())
-    .then(setCurrentFilm)
-  }
-  
   return (
-  
     <div className="App">
-      <form>
-        <input type='text' placeholder="Search..." name="filmsearch" value={filmSearch} onChange={handleFilmSearch}></input>
-        <button type="submit" onClick={handleFilmSearchSubmit}>Search</button>
-      </form>
-      <img src={currentFilm.Poster} alt="poster"></img> 
+      <Search />
     </div>
-  );
+  )
 }
 
 export default App;
--- p2groupproj/src/components/FilmDetail.js
+++ p2groupproj/src/components/FilmDetail.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function FilmDetail () {
+function FilmDetail ({film}) {
+    console.log(film)
     return(
-        <div></div>
+        <div>
+            <h3>{film.Title}</h3>
+            <img src={film.Poster} alt="poster"></img> 
+            <p>{film.Year} • Directed by {film.Director}</p>
+            <p>Starring {film.Actors}</p>
+            <p>Plot Summary: {film.Plot}</p>
+            <button>Add to Watchlist</button>
+        </div>
     )
 }
 
