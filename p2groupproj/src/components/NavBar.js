e2bb17a84fa32d321a1028650b3a0a971ae1d80b
Daniel Magers <daniel.magers@gmail.com>
2022-08-23 10:17:16 -0500

    css experiments

--- p2groupproj/src/components/NavBar.js
+++ p2groupproj/src/components/NavBar.js
@@ -5,9 +5,9 @@ import { NavLink } from "react-router-dom"
 function NavBar () {
     return(
         <div className="navbar">
-            <NavLink exact to ="/">Home</NavLink>
-            <NavLink to="/yourfilmlist">Your Film List</NavLink>
-            <NavLink to="/addfilms">Add Films to List</NavLink>
+            <NavLink exact to ="/"> Home </NavLink>
+            <NavLink to="/yourfilmlist"> Your Film List </NavLink>
+            <NavLink to="/addfilms"> Add Films to List </NavLink>
         </div>
     )
 }
