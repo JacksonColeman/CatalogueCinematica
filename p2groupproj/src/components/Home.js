7d8c926d281019aea741cfadec43eeda350bcd85
Daniel Magers <daniel.magers@gmail.com>
2022-08-24 08:20:06 -0500

    home page css

--- p2groupproj/src/components/Home.js
+++ p2groupproj/src/components/Home.js
@@ -3,7 +3,51 @@ import React from 'react'
 function Home () {
     return(
         <div>
+            <div className="row">
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Snow"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Snow"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Forest"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" alt="Mountains"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Snow"/>
+            </div>
+            </div>
+            
+            <div className="row">
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Snow"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Forest"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" alt="Mountains"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Snow"/>
+            </div>
+            <div className="column">
+                <img className="home-pic" src="https://m.media-amazon.com/images/M/MV5BMTAxNDA1ODc5MDleQTJeQWpwZ15BbWU4MDg2MDA4OTEx._V1_SX300.jpg" 
+                alt="Snow"/>
+            </div>
+            </div>
             <p>This Project is to Track Films You Want to Watch</p>
+
         </div>
     )
 }
